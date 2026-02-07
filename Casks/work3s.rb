4df4547721fc37b65bcd8db9dd066199acd3e6b7
Eddy Ferreira <79837100+EddyFromPoker@users.noreply.github.com>
2026-02-07 22:00:50 +0000

    Update work3s to v0.3.12

--- Casks/work3s.rb
+++ Casks/work3s.rb
@@ -1,11 +1,11 @@
 cask "work3s" do
-  version "0.3.2"
-  sha256 "80608ff4998f8a19eacbf9bdac9a874d3ae4badd422a98eec79f31bbc592041d"
+  version "0.3.12"
+  sha256 "cb9464aa8d8aae644e287c0367a96bd56a2d9846c2c7fb253b1881fc0a93709d"
 
-  url "https://github.com/EddyFromPoker/work3s-desktop/releases/download/v#{version}/Work3s_#{version}_aarch64.dmg"
+  url "https://github.com/EddyFromPoker/work3s/releases/download/v#{version}/Work3s_#{version}_aarch64.dmg"
   name "Work3s"
   desc "Git worktree management for macOS"
-  homepage "https://github.com/EddyFromPoker/work3s-desktop"
+  homepage "https://github.com/EddyFromPoker/work3s"
 
   livecheck do
     url :url

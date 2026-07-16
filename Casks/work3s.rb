afb4aad6e4f2fb94f63a85fb36dd8f7fa23bdd28
EddyFromPoker <eddyfrompoker@gmail.com>
2026-07-16 02:32:31 +0100

    Update work3s to v0.5.36

--- Casks/work3s.rb
+++ Casks/work3s.rb
@@ -1,6 +1,6 @@
 cask "work3s" do
-  version "0.5.35"
-  sha256 "919b883e3c810bea379128c25584431f8237e014d93120e8169f2c6553187d16"
+  version "0.5.36"
+  sha256 "a0ba9d07fe659d276900d6184220cd23aece75ef7ebb99921f99fcbfbe1af99e"
 
   url "https://github.com/work3s/desktop/releases/download/v#{version}/Work3s_#{version}_aarch64.dmg"
   name "Work3s"
